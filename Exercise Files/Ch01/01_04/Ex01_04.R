ce2d1b87c6f5c8aeea00624a873a2385a63678ad
Loretta Agyemang <loretta.agyemang@gmail.com>
2017-10-04 17:35:33 -0400

    finished exercise 1.4

--- Exercise Files/Ch01/01_04/Ex01_04.R
+++ Exercise Files/Ch01/01_04/Ex01_04.R
@@ -2,3 +2,27 @@
 # Ex01_04
 # Installing and managing packages
 
+# LISTS OF PACKAGES
+browseURL("http://cran.r-project.org/web/views")
+browseURL("http://crantastic.org")
+
+# Sww xueewnr packages
+library()
+search()
+
+# Install pacakges 
+?install.packages
+install.packages("ggplot2")  # Make package available; for loading in scripts
+require("ggplot2") # preferred for loading in functions but can also use library()
+
+# Documantation for packages
+library(help = "ggplot2")
+
+# Bring up examples of code 
+vignette()
+
+browseVignettes(package = "grid")
+
+# Update packages - checks for updates
+update.packages(y)
+y
