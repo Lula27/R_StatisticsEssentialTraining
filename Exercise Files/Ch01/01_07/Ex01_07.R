f970d380b75f512170e30de698b15087f4a30d62
Loretta Agyemang <loretta.agyemang@gmail.com>
2017-10-04 18:25:56 -0400

    stuck on excercise 1.7 - won't read file path

--- Exercise Files/Ch01/01_07/Ex01_07.R
+++ Exercise Files/Ch01/01_07/Ex01_07.R
@@ -1,3 +1,18 @@
 # R Statistics Essential Training
 # Ex01_07
 # Importing data
+
+# Don't export dirctly from Excel - use CSV or text files 
+# This command gives an error with missging data but 
+# Works well on complete data - no empty spaces
+trends.txt <- read.table("C:/Users/loret/Desktop/DataSciencePrep/R_StatisticsEssentialTraining/Exercise Files/Ch01/01_07/Final/GoogleTrends")
+
+# above not working...wonder why
+
+?read.table
+
+str(trends.txt)
+
+# CSV Files 
+trends.csv <- read.csv("C:\\Users\\loret\\Desktop\\DataSciencePrep\\R_StatisticsEssentialTraining\\Exercise Files\\Ch01\\01_07\\Final\\GoogleTrends",
+                       header = TRUE)
