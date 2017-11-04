bec0aabd4b35d3546fc37aa86f58f9028255bc02
Loretta Agyemang <loretta.agyemang@gmail.com>
2017-11-04 15:33:37 -0400

    reading text and csv files

--- Exercise Files/Ch01/01_07/Ex01_07.R
+++ Exercise Files/Ch01/01_07/Ex01_07.R
@@ -1,18 +1,30 @@
 # R Statistics Essential Training
 # Ex01_07
-# Importing data
+# Importing data - use CSV files! 
 
 # Don't export dirctly from Excel - use CSV or text files 
 # This command gives an error with missging data but 
 # Works well on complete data - no empty spaces
-trends.txt <- read.table("C:/Users/loret/Desktop/DataSciencePrep/R_StatisticsEssentialTraining/Exercise Files/Ch01/01_07/Final/GoogleTrends")
 
-# above not working...wonder why
+
+# Read data file/table 
+# Missing values - Won't read because there are empty spaces, use fill = TRUE
+trends.txt <- read.table("C:\\Users\\loret\\Desktop\\DataSciencePrep\\R_StatisticsEssentialTraining\\Exercise Files\\Ch01\\01_07\\GoogleTrends.txt", fill = TRUE, 
+                         header = TRUE)
+
+trends.txt
+
 
 ?read.table
 
+# Retreive structure of text file 
 str(trends.txt)
 
 # CSV Files 
-trends.csv <- read.csv("C:\\Users\\loret\\Desktop\\DataSciencePrep\\R_StatisticsEssentialTraining\\Exercise Files\\Ch01\\01_07\\Final\\GoogleTrends",
+# With CSV files, can read even with missing values
+trends.csv <- read.csv("C:\\Users\\loret\\Desktop\\DataSciencePrep\\R_StatisticsEssentialTraining\\Exercise Files\\Ch01\\01_07\\GoogleTrends.csv",  
                        header = TRUE)
+trends.csv
+
+str(trends.csv)
+View(trends.csv)
