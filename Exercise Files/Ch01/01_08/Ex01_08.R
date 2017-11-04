78e7725ddc85febaa645d83ebfe79a5773f865eb
Loretta Agyemang <loretta.agyemang@gmail.com>
2017-11-04 17:45:49 -0400

    created funtion

--- Exercise Files/Ch01/01_08/Ex01_08.R
+++ Exercise Files/Ch01/01_08/Ex01_08.R
@@ -28,4 +28,49 @@ admin.fail <- (UCBAdmissions$Admit)
 # $ operator is invalid for atomic vectors
 # Atomic vector = simplest R data type; linear vectors of a single primitive type
 
-# 
+# Try generating barplot - won't work
+barplot(UCBAdmissions$Admit)
+# Try scatter plot - worked but hard to read 
+plot(UCBAdmissions)
+
+
+# Get marginal frequencies from original table 
+?margin.table
+margin.table(UCBAdmissions, 1) # Admit
+margin.table(UCBAdmissions, 2) # Gender
+margin.table(UCBAdmissions, 3) # Department
+margin.table(UCBAdmissions) # Grand Total
+
+# Save margins as new table
+admin.dept <- margin.table(UCBAdmissions, 3)
+str(admin.dept)
+# graph data w/ barplot 
+barplot(admin.dept)
+admin.dept # Show frequencies
+prop.table(admin.dept) # Show as proportions 
+# use round function - round() to reduce decimal places - to two decimals
+round(prop.table(admin.dept), 2)
+round(prop.table(admin.dept), 2)*100 # attempt to get percents
+?prop.table
+?round
+
+# Step 3 - Go from table to one row per case
+admit1 <- as.data.frame(UCBAdmissions) # Coerces to data frame
+# Change to a list - Repeats each row by Freq
+admit2 <- lapply(admit1, function(x)rep(x, admit1$Freq))
+admit2
+# Change list back to data frame
+admit3 <- as.data.frame(admit2)
+# remove frequency column (redundant info) 
+admit4 <- admit3[, -4]
+
+# Funtion to do everything at once 
+admit.rows <- as.data.frame(lapply(as.data.frame.table
+(UCBAdmissions), function(x)rep(x, as.data.frame.table
+(UCBAdmissions)$Freq)))[, -4]
+
+admit.rows
+
+# Created simple adding function 
+add <- function(x)(x + 1)
+add(4)
