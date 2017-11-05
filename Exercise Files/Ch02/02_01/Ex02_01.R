ca2e2727a4d4b21be752f71c97719fc8d4dc86e7
Loretta Agyemang <loretta.agyemang@gmail.com>
2017-11-05 18:32:22 -0500

    plot feed data from chickwts dataset

--- Exercise Files/Ch02/02_01/Ex02_01.R
+++ Exercise Files/Ch02/02_01/Ex02_01.R
@@ -1,3 +1,28 @@
 # R Statistics Essential Training
 # Ex02_01
 # Creating bar charts for categorical variables
+# Basic graph for categorical variables = bar chart 
+
+# PART 1 
+# LOAD DATASETS PACKAGE
+datasets::chickwts
+
+# ONE ROW PER CASE
+?chickwts
+
+# Look at data
+chickwts
+
+# Load data into workspace
+?data
+data(chickwts)
+
+# Quickest Method - Plot feed from chickwts
+plot(chickwts)
+
+# Plot after selecting the variable feed 
+plot(chickwts$feed)
+
+
+# PART 2 
+
