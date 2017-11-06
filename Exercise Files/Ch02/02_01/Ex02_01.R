40343b3a081ba1000b16dee2ab7f6ed1dbbe5963
Loretta Agyemang <loretta.agyemang@gmail.com>
2017-11-05 19:12:59 -0500

    ordered barplot variables

--- Exercise Files/Ch02/02_01/Ex02_01.R
+++ Exercise Files/Ch02/02_01/Ex02_01.R
@@ -24,5 +24,21 @@ plot(chickwts)
 plot(chickwts$feed)
 
 
-# PART 2 
+
+
+# PART 2 - Customize with your plot - jazzzzz it up!
+# Create variable as a table with the feeds variable 
+feeds <- chickwts$feed 
+rm(feeds)
+
+# Create variable as a table with the feeds variable 
+feeds <- table(chickwts$feed)
+feeds
+
+# Order decreasing
+barplot(feeds[order(feeds, decreasing = TRUE)])
+
+# Customize the chart
+# Sets outside margins: b, l, t, r 
+
 
