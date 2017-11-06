04e59f5cbad9d6c04881f73d06e3f23d865ec556
Loretta Agyemang <loretta.agyemang@gmail.com>
2017-11-05 19:57:17 -0500

    going to do my own with a categorical data set

--- Exercise Files/Ch02/02_01/Ex02_01.R
+++ Exercise Files/Ch02/02_01/Ex02_01.R
@@ -40,5 +40,28 @@ barplot(feeds[order(feeds, decreasing = TRUE)])
 
 # Customize the chart
 # Sets outside margins: b, l, t, r 
+?par
+# oma = vector of form c(bottom, left, top, right) giving size of outer margins in lines of text
+par(oma = c(1, 1, 1, 1))
+# mar = numerical vector of form c(bottom, left, top, right) which gives # of margin to be 
+# specified on four sides of plot
+par(mar = c(4, 5, 2, 1))
+
+# create barplot
+# \n is line break like <br> in html 
+?barplot 
+barplot(feeds[order(feeds)],
+        horiz  = TRUE,
+        las    = 1,  # las gives orientation of axis labels
+        col    = c("beige", "blanchedalmond", "bisque1", "bisque2", "bisque3", "bisque4"),
+        border = NA,  # No borders on bars
+        main   = "Frequencies of Different Feeds\nin chickwts Dataset",  # \n = line break
+        xlab   = "Number of Chicks"
+        
+        )
 
 
+
+# Replicate with my own
+Start here: http://rstudio-pubs-static.s3.amazonaws.com/2277_404d9b623e0b4dcf9fe0927c38067813.html 
+datasets::attitude
