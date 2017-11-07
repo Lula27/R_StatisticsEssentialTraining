ad5d6965a6085277254eac65f0ebd4be1ed8406c
Loretta Agyemang <loretta.agyemang@gmail.com>
2017-11-07 16:15:15 -0500

    completed ttest to test mean of magnitude variable in quakes data

--- Exercise Files/Ch03/03_04/Ex03_04.R
+++ Exercise Files/Ch03/03_04/Ex03_04.R
@@ -1,3 +1,49 @@
 # R Statistics Essential Training
 # Ex03_04
 # Single mean: Hypothesis test and confidence interval
+
+
+# Load data - look at structure 
+?quakes
+str(quakes)
+
+
+# See the first 5 lines of the data
+quakes[1:5, ]
+
+
+# Just load the magnitude variable
+quakes$mag[1:5]
+
+
+
+# Select magnitude for entire data set - store in object called mag
+mag <- quakes$mag
+mag
+
+# Select first five rows for mag & notice the different in t-test results 
+mag2 <- quakes$mag[1:5]
+
+# Use t-test for one-sample
+# Default t-test (compares mean to 0)
+# Ho: true mean = 0 
+# Ha: true mean != 0 
+?t.test()
+t.test(mag) # T-test for entire sample of magnitudes
+# Notice for this natural science example, that the t-test is huge; for social sciences, much smaller t-tests (around single digits)
+
+t.test(mag2) # T-test for sample of first 5 observations 
+
+# right One-sided t-test w/ true mean(mu) = 4 as Ho 
+t.test(mag, alternative = "greater", mu = 4)
+
+
+
+# Clean up
+rm(list = ls())
+
+
+
+
+
+
--- Exercise Files/Ch03/03_04/Final/Ex03_04.R
+++ Exercise Files/Ch03/03_04/Final/Ex03_04.R
@@ -4,15 +4,19 @@
 
 # Load data
 ?quakes
+str(quakes)
 quakes[1:5, ]  # See the first 5 lines of the data
 mag <- quakes$mag  # Just load the magnitude variable
 mag[1:5]  # First 5 lines
 
 # Use t-test for one-sample
 # Default t-test (compares mean to 0)
+# This was kind of useless since the mean for the variable mag is around 4. No low values of around 0 even recorded.
 t.test(mag)
 
-# One-sided t-test w/mu = 4
+# Fine tune it for what you need to look at 
+# One-sided t-test w/mu = 4 
+# Population mean (true mean) = 4 
 t.test(mag, alternative = "greater", mu = 4)
 
 rm(list = ls())  # Clean up
