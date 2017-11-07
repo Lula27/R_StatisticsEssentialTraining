714b2166fb9759d2ceb29d14050002befbd8ddce
Loretta Agyemang <loretta.agyemang@gmail.com>
2017-11-07 15:41:40 -0500

    completed inferential statistics with single proportion

--- Exercise Files/Ch03/03_03/Ex03_03.R
+++ Exercise Files/Ch03/03_03/Ex03_03.R
@@ -1,3 +1,40 @@
 # R Statistics Essential Training
 # Ex03_03
+# Following descriptive statistics/frequencies, conduct inferential statistics 
+# Use basic inferential statistic - Single Proportion 
 # Single proportion: Hypothesis test and confidence interval
+# Extra reading: https://stats.idre.ucla.edu/other/mult-pkg/faq/general/faq-what-are-the-differences-between-one-tailed-and-two-tailed-tests/ 
+
+
+# In the 2012 Major League Baseball season,
+# the Washington Nationals had the best record 
+# at the end of the regular season: 98 wins out
+# of 162 games (.605). Is this significantly 
+# greater than 50%? 
+
+# Generate hypotheses 
+# Ho = no significant difference (no difference) / true p equals 0.5
+# Ha = significant different / true p not equal to 0.5 
+
+# (trials = n) total games = 162
+# (successes = x) number of wins = 98
+# Problem: Is their record for wins (98/162 = .605)
+# significantly greater than 50%? 
+
+# PROP TEST - test of equal/given proportions 
+# 98 wins out of 162 games 
+# (default settings) Default = null probability = 0.5 
+# 95% CI; 50/50 coin toss 
+?prop.test
+prop.test(98, 162)
+
+# output: p-value = 0.009522 / < 1%     -> statisitcally significant so reject null hypothesis (doesn't mean accept Ha)
+
+
+
+# One-tailed test with 90% CI - right sided 
+prop.test(98, 162, alternative = "greater", conf.level = .90)
+
+
+# Clean up 
+
--- Exercise Files/Ch03/03_03/Final/Ex03_03.R
+++ Exercise Files/Ch03/03_03/Final/Ex03_03.R
@@ -6,10 +6,17 @@
 # the Washington Nationals had the best record 
 # at the end of the regular season: 98 wins out
 # of 162 games (.605). Is this significantly 
-# greater than 50%?
+# greater than 50%? 
 
-# PROP TEST
+# total games = 162
+# number of wins = 98
+# Problem: Is their record for wins (98/162 = .605)
+# significantly greater than 50%? 
+
+# PROP TEST - test of equal/given proportions 
 # 98 wins out of 162 games (default settings)
+?prop.test
+#prop.test(x = counts of successes, n = vector of counts of trials)
 prop.test(98, 162)
 
 # One-tailed test with 90% CI
