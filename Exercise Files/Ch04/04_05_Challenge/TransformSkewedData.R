a96e032bcb358a95830e0448a66da459c3134b5e
Loretta Agyemang <loretta.agyemang@gmail.com>
2017-11-10 12:12:25 -0500

    best attempt at muting outliers is a log transformation

--- Exercise Files/Ch04/04_05_Challenge/TransformSkewedData.R
+++ Exercise Files/Ch04/04_05_Challenge/TransformSkewedData.R
@@ -14,7 +14,7 @@ x <- skdata$x
 hist(x)
 
 require(psych)
-kurtosi(x)
+kurtosi(x)   # = 2.300297
 boxplot(x)
 boxplot.stats(x)
 
@@ -25,13 +25,19 @@ boxplot(x.z)
 
 
 
-# Log transform - hmmm...still nothing 
-x.log <- log(x)
+# Log transform - hmmm...beter. Outliers shrunk down by like a factor of four <- This right here is my best bet. 
 hist(x.log)
 
 boxplot(x.log)
-
+boxplot.stats(x.log)
+kurtosi(x.log) # Why is this so high? 26.45631
 length(x.log)
+
+# Now try scaling x.log
+z.log = scale(x.log)
+hist(z.log)
+boxplot(z.log)
+boxplot.stats(z.log) # nope, it's even worse than after the log transformation 
 
 # Try multiplying by rnorm - but outliers still present 
 ?rnorm
