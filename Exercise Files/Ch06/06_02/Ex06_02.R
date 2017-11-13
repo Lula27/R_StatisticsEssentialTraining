2df75170a5b67900cf57359943232df688d8e7c1
Loretta Agyemang <loretta.agyemang@gmail.com>
2017-11-13 17:56:48 -0500

    created outline of analysis using grouped boxplot

--- Exercise Files/Ch06/06_02/Ex06_02.R
+++ Exercise Files/Ch06/06_02/Ex06_02.R
@@ -1,3 +1,17 @@
 # R Statistics Essential Training
 # Ex06_02
 # Creating grouped boxplots
+
+
+# Load data
+# Use dataset "painters" from the package "MASS
+require(MASS)
+?painters
+
+data("painters")
+painters
+
+
+# Draw boxplots of outcome (Expression) by group (School)
+# Basic version - not very helpful so fancy it up! 
+boxplot(painters$Expression ~ painters$School)
