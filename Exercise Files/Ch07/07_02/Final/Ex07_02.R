f7e67215eed4ace7988e175ac41c8aad38a2f949
Loretta Agyemang <loretta.agyemang@gmail.com>
2017-11-15 15:18:11 -0500

    compared avarage extra hours of sleep for group one and two after taking sleeping pill using t-test

--- Exercise Files/Ch07/07_02/Final/Ex07_02.R
+++ Exercise Files/Ch07/07_02/Final/Ex07_02.R
@@ -3,17 +3,19 @@
 # Computing a bivariate regression
 
 # Load data
-?trees
+?trees # Girth, height & volume for black cherry trees 
 data(trees)
 trees[1:5, ]  # Show first 5 lines
 
 # Quick graphical check on data
 hist(trees$Height)
-hist(trees$Girth)
+hist(trees$Girth) # girth = diameter of tress in inches measured at 4ft 6in above ground 
 plot(trees$Girth, trees$Height)
 abline(lm(trees$Height ~ trees$Girth))
 
-# Linear regression model
+# Linear regression model - bivariate regression
+# height(y) = bo + b1girth(x) + res 
+# Explain height using Girth
 reg1 <- lm(Height ~ Girth, data = trees)
 reg1
 summary(reg1)
@@ -30,3 +32,4 @@ lm.influence(reg1)
 influence.measures(reg1)
 
 rm(list = ls())  # Clean up
+dev.off()
--- Exercise Files/Ch07/07_03/Ex07_03.R
+++ Exercise Files/Ch07/07_03/Ex07_03.R
@@ -1,3 +1,65 @@
 # R Statistics Essential Training
 # Ex07_03
-# Comparing means with the t-test
+# Comparing means with the t-test: one of the most useful tests for comparing accross groups 
+# Part one - use t-test to test for difference in means between 2 groups in small data set on sleep medication
+# Part two - generate your own small dataset using random - use t-test to test for statistically significant different means
+
+
+# Pt 1 
+# Load data
+?sleep
+data(sleep)
+sleep[1:10, ]
+
+
+# Save just the first two variables - just looking at extra & group (take Id out) - call it sd (sleep drug)
+sd <- sleep[ ,1:2]
+sd[1:10, ]
+
+# Some quick plots to check data
+hist(sd$extra)
+hist(sd$extra, col = "darkblue") # - missing bin - coloring  will help you see it
+# boxplot extra hours of sleep based on group - boxplot(a ~ b, data = name)
+boxplot(extra ~ group, data = sd) # looks like there is a huge difference between two groups
+# Group one on average has less extra hours of sleep compared to group 2
+# looks like factor of 2 difference in hours of sleep recorded in grp 2 compared to grp 1
+
+# Is the difference I'm noticing in boxplots statisitically significant? 
+# Perform t-tests to see if the mean extra sleep are different in the groups
+# Independent 2-group t-test (with defaults) - t.test(a ~ b, data = name)
+t.test(extra ~ group, data = sd) # Welch Two Sample t-test, no direction; 95% CI 
+# p-value = 0.07939 - not statitically significant at 5% level
+# This may be because it's such a small sample size.
+
+
+# Notice how it changes when you specify direction. 
+# Test to see if one of the means is less than the other at 80% CI. 
+# t-test with options - one-tailed
+t.test(extra ~ group,
+       data = sd,
+       alternative = "less",  # One-tailed test
+       conf.level = 0.80)  # 80% CI (vs. 95%)
+# Now, there is a stat. sig. p-val @ 5% <- p-value = 0.0397
+# CI just telling you that there are no bounds on one side - graph going from -inf to about -0.848
+
+
+
+# Part 2 - pedogogical testing: because generating values randomly, never have the same sample
+# Create two groups of random data in separate variables
+# Good because actual difference is known
+# Size = 30 , mean = 20, sd = 5
+x <- rnorm(30, mean = 20, sd = 5)
+x[1:10]
+# Same size & sd, different means 
+y <- rnorm(30, mean = 25, sd = 5)
+y[1:10]
+
+# Test difference in means using one-sided t-test
+t.test(x,y)
+
+# Looks like there is statistical signicance to reject ho that the means are the same. 
+# This could be because you now have a random sample. 
+
+# Clean up 
+rm(list = ls())
+dev.off()
