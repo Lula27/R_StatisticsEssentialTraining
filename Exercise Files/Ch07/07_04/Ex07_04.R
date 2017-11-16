5377dd6a11150f5b3312323e54a40562f4cc1b96
Loretta Agyemang <loretta.agyemang@gmail.com>
2017-11-16 14:45:41 -0500

    drew samples and compared means before and after using paired ttesting

--- Exercise Files/Ch07/07_04/Ex07_04.R
+++ Exercise Files/Ch07/07_04/Ex07_04.R
@@ -1,3 +1,58 @@
 # R Statistics Essential Training
 # Ex07_04
 # Comparing paired means: Paired t-test
+
+# Load data
+# Create random data sampled from random normal distribution 
+# rnorm(observation, mean = mu, sd = x) <- time 1
+time1 <- rnorm(100, mean = 50, sd = 10)
+
+# Simulate change over time - before & after
+# Difference - same observations, smaller mean/ greater sd
+diff <- rnorm(100, mean = 10, sd = 20)
+
+# Create sum vector 
+time2 <- time1 + diff  # = rnorm(100, 60, 30)
+
+
+# Plot histograms of three vectors/ boxplot of time 1 & time 2
+hist(time1) # slight right skew
+hist(diff) # looks more normal than time1
+hist(time2) # kind of a left skew?
+
+
+# Save variables in dataframe and use "MASS"
+# to create parallel coordinate plot
+# Get dataset into single data frame - newdf <- data.frame(t1, t2)
+timeChange <- data.frame(time1, time2)
+require(MASS) # For some reason, even though I can successfully run require(MASS), the package doesn't have check near name
+
+# Crate parallel coordinate plot
+# parcoord(paired dataframe, var.label = TRUE) # var.label will label min & max
+?parcoord
+parcoord(timeChange, var.label = TRUE) # looks like an increase - more stark than in example 
+
+
+# Now that we have a visualization of patterns, let's do some testing!
+# Paired t-test (with defaults)
+?t.test
+t.test(time1,time2, paired = TRUE) # p-value = 2.779e-05; incredibly strong evidence for statistically sig. different means
+t.test(time2,time1, paired = TRUE) # p-value is still the same but it looks like positive values for t & CI 
+# Makes sense...going from time1(less) to time2(more) so negative difference in the first t.test
+
+# Paired t-test with options
+# Check if significantly greater than 10 (difference mean)
+t.test(time2, time1, 
+       paired = TRUE,
+       mu = 10,  # Specify non-0 null value - mu = population mean
+       alternative = "greater",  # One-tailed test
+       conf.level = 0.99)  # 99% CI (vs. 95%)
+
+# Not statitically significance 
+# mean of the differences = 9.684581
+
+
+# Clean up
+detach("package:MASS", unload=TRUE) 
+rm(list = ls())
+dev.off()
--- Exercise Files/Ch07/07_04/Final/Ex07_04.R
+++ Exercise Files/Ch07/07_04/Final/Ex07_04.R
@@ -1,35 +1,49 @@
 # R Statistics Essential Training
 # Ex07_04
-# Comparing paired means: Paired t-test
+# Comparing paired means: Paired t-test - compare one group before & after
 
 # Load data
 # Create random data
 t1 <- rnorm(50, mean = 52, sd = 6)  # Time 1
+
+# Simulate change over time - before & after
 dif <- rnorm(50, mean = 6, sd = 12)  # Difference
+# Vector math
 t2 <- t1 + dif  # Time 2
+t2
 
 # Some quick plots to check data
-hist(t1)
+hist(t1) # basically normal - sampled from random normal distribution
 hist(dif)
 hist(t2)
 boxplot(t1, t2)
 
 # Save variables in dataframe and use "MASS"
 # to create parallel coordinate plot
+# Get dataset into single data frame
 pairs <- data.frame(t1, t2)
+pairs
 require("MASS")
-parcoord(pairs, var.label = TRUE)
+
+# Crate parallel coordinate plot
+parcoord(pairs, var.label = TRUE) # var.label will label min & max
+# Look to see if on average, lines relating t1 & t2 are going up, down or stay flat
 
 # Paired t-test (with defaults)
-t.test(t2, t1, paired = TRUE)
+t.test(t2, t1, paired = TRUE) # strongly significant 
+# Since there was a change in scores (huge diff. btw means) when we first 
+# generated sample, we expect this.
 
 # Paired t-test with options
+# Check if significantly greater than 6 (mean we sampled from)
 t.test(t2, t1, 
        paired = TRUE,
-       mu = 6,  # Specify non-0 null value
+       mu = 6,  # Specify non-0 null value - mu = population mean
        alternative = "greater",  # One-tailed test
        conf.level = 0.99)  # 99% CI (vs. 95%)
+# Not statistically significant
 
 # Clean up
 detach("package:MASS", unload=TRUE)
 rm(list = ls())
+dev.off()
