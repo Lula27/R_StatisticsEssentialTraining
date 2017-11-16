a02f10ea2ad531cba8b65e3b65be6313eb501055
Loretta Agyemang <loretta.agyemang@gmail.com>
2017-11-16 15:32:05 -0500

    conducted anova and post hoc comparrison with Tukey

--- Exercise Files/Ch07/07_05/Ex07_05.R
+++ Exercise Files/Ch07/07_05/Ex07_05.R
@@ -1,3 +1,46 @@
 # R Statistics Essential Training
 # Ex07_05
 # Comparing means with ANOVA
+
+
+
+# Load data - Create 4 vectors drawing from random normal distribution. 
+# Same cases/number of observations, similar means, same sd.
+# Each group in separate variable
+a <- rnorm(50, mean = 70, sd = 7)
+b <- rnorm(50, mean = 68, sd = 7)
+c <- rnorm(50, mean = 72, sd = 7)
+d <- rnorm(50, mean = 70, sd = 7)
+
+
+# Combine vectors into a single data frame
+# Use cbind to combine columns so you can create new dataframe composed of those 4 individual vectors
+# data.frame(cbind()) 
+alp <- data.frame(cbind(a,b,c,d))
+
+
+
+# In order to do ANOVA, need to stack up the cbind dataframe. Results in values & ind
+# Stack data to get one column with outcome
+# and second columns with group
+# Stacking vectors concatenates multiple vectors into a single vector along with a factor indicating where each observation originated.
+?stack
+alphabet <- stack(alp) # Notice the number of observations between dataframe(50) & stacked df(200) = 50 * 4
+
+
+
+# Conduct one-way ANOVA (Analysis of Variance) 
+# aov(outcome ~ ind, data = stacked dfname )
+# anova1 <- aov(values ~ ind, data = xs)
+anova1 <- aov(values ~ ind, data = alphabet)
+anova1  # Not statitically significant... 
+summary(anova1)
+
+# Post-hoc comparisons - Shows where difference is.
+# Can use number of tests [Bonferroni,etc.] This instructor likes Tukey cus he thinks others are conservative estimates.
+?TukeyHSD
+TukeyHSD(anova1)
+
+
+# Clean up - Go over this again!
+rm(list = ls())
--- Exercise Files/Ch07/07_05/Final/Ex07_05.R
+++ Exercise Files/Ch07/07_05/Final/Ex07_05.R
@@ -6,25 +6,32 @@
 # Each group in separate variable
 x1 <- rnorm(30, mean = 40, sd = 8)
 x2 <- rnorm(30, mean = 41, sd = 8)
-x3 <- rnorm(30, mean = 45, sd = 8)
+x3 <- rnorm(30, mean = 45, sd = 8) # You'll get different samples for x3 & x4 even though the parameters appear the same b/c drawing from random normal distribution.
 x4 <- rnorm(30, mean = 45, sd = 8)
 # Formula result is F(3, 116) = 3.24, p = .025
-boxplot(x1, x2, x3, x4)  # Quick graphical check
+
+boxplot(x1, x2, x3, x4)  # Quick graphical check # Why are the means for the boxplot of x3 & x4 different? 
+
 # Combine vectors into a single data frame
+# Use cbind to combine columns so you can create new dataframe composed of those 4 individual vectors
 xdf <- data.frame(cbind(x1, x2, x3, x4))
 summary(xdf)
+
 # Stack data to get one column with outcome
 # and second columns with group
-xs <- stack(xdf)
+xs <- stack(xdf) # In order to do ANOVA, need to stack up the cbind dataframe. Results in values & ind
 
-# Conduct one-way ANOVA
+# Conduct one-way ANOVA (Analysis of Variance) 
+# aov(outcome ~ ind, data = stacked dfname )
 anova1 <- aov(values ~ ind, data = xs)
 anova1
-summary(anova1)
+summary(anova1) # Summary tells us there is a statistically significant difference but doesn't say where.
 
-# Post-hoc comparisons
+# Post-hoc comparisons - Shows where difference is.
+# Can use number of tests [Bonferroni,etc.] This instructor likes Tukey cus he thinks others are conservative estimates.
 TukeyHSD(anova1)
 ?pairwise.t.test  # Other post-hoc tests
 ?p.adjust  # Specific methods
 
 rm(list = ls())  # Clean up
+dev.off()
