b7e23a42f0f4a0c71971ae3f8f92234cb6935c70
Loretta Agyemang <loretta.agyemang@gmail.com>
2017-11-20 11:34:16 -0500

    multivariate plotting with grouped bar chart of means

--- Exercise Files/Ch08/08_01/Ex08_01.R
+++ Exercise Files/Ch08/08_01/Ex08_01.R
@@ -1,3 +1,30 @@
 # R Statistics Essential Training
 # Ex08_01
 # Creating clustered bar chart for frequencies
+
+# Goal: see how often different types of wool break under tension
+# Three variables 
+# Outcome: number of breaks <- type of wool (A or B) * tension factor (L,M,H)
+
+
+# Load data
+# Built-in dataset "warpbreaks"
+?warpbreaks
+# loom = fixed length of yarn 
+# warp breaks = number of breaks in yarn during weaving 
+data("warpbreaks")
+str(warpbreaks)
+
+
+# Doesn't work: Creating first barplot with breaks(output) as 
+# an interaction of wool & tension
+barplot(breaks ~ wool*tension, data = warpbreaks)
+# Result: Error in barplot.default(breaks ~ wool * tension, data = warpbreaks) : 
+# 'height' must be a vector or a matrix
+
+
+# Restructure dataset to work with a different barplot formatting 
+?tapply # apply a function over a ragged array - 
+# Apply a function to each cell of a ragged array, that is to each (non-empty) 
+# group of values given by a unique combination of the levels of certain factors.
+
--- Exercise Files/Ch08/08_01/Final/Ex08_01.R
+++ Exercise Files/Ch08/08_01/Final/Ex08_01.R
@@ -1,30 +1,43 @@
 # R Statistics Essential Training
 # Ex08_01
-# Creating clustered bar chart for frequencies
+# Creating clustered bar chart for frequencies (one t)
+# Build grouped bar chart from means
+# Show association btw two categorical predictor variables and 
+# a single quantitative outcome - common approach in reasearch 
+# Goal: see how often different types of wool break under tension
+# Three variables 
+# Outcome: number of breaks <- type of wool (A or B) * tension factor (L,M,H)
 
 # Load data
 # Built-in dataset "warpbreaks"
 ?warpbreaks
+# loom = fixed lenght of yarn 
+# warp breaks = number of breaks in yarn during weaving 
+data("warpbreaks")
+warpbreaks
+
 # Doesn't work:
-barplot(breaks ~ wool*tension, data = warpbreaks)
+barplot(breaks ~ wool*tension, data = warpbreaks) # see if number of breaks is a product of wool & tension factors - doesn't work
 
+# Restructure data so this will work - creates tables dataset 
 data <- tapply(warpbreaks$breaks,
-               list(warpbreaks$wool,
+               list(warpbreaks$wool, # list two predictors
                     warpbreaks$tension),
-               mean)
+               mean) # getting mean from dataset 
 
-barplot(data,
-        beside = TRUE,
-        col = c("steelblue3", "thistle3"),
-        bor = NA,
-        main = "Mean Number of Warp Breaks\nby Tension and Wool",
-        xlab = "Tension",
+barplot(data, # dataset
+        beside = TRUE, # group factors - right next to each other
+        col = c("steelblue3", "thistle3"), # colors
+        bor = NA, # Get rid of borders 
+        main = "Mean Number of Warp Breaks\nby Tension and Wool", # main title
+        # Chart labels
+        xlab = "Tension", 
         ylab = "Mean Number of Breaks")
 
 # For legend, "locator(1)" is interactive and lets you click
 # where you want to put the legend. You can also specify
 # with coordinates.
-legend(locator(1),
+legend(locator(1), # Allows you to dynamically/interactively add legend 
        rownames(data),
        fill = c("steelblue3", "thistle3"))
 
