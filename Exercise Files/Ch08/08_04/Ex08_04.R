75b37c1174229714ceb5ba1529f97c13bb028f32
Loretta Agyemang <loretta.agyemang@gmail.com>
2017-11-21 15:35:35 -0500

    worked with 3-d scatterplot for a bit

--- Exercise Files/Ch08/08_04/Ex08_04.R
+++ Exercise Files/Ch08/08_04/Ex08_04.R
@@ -22,3 +22,35 @@ scatterplot3d(iris[1:3])
 # Modified static 3D scatterplot - helps to better place 
 # Coloring, vertical lines
 # and Regression Plane - saved as an object
+s3d <- scatterplot3d(iris[1:3],
+                   pch = 17,
+                   highlight.3d = TRUE,
+                   type = "h",
+                   main = "3D Scatterplot")
+# create plane
+plane <- lm(iris$Petal.Length ~ iris$Sepal.Length + iris$Sepal.Width)
+s3d$plane3d(plane)
+
+# Spinning 3D scatterplot
+# Install and load the "rgl" package ("3D visualization 
+# device system (OpenGL)")
+# NOTE: This will cause RStudio to crash when graphics 
+# window is closed....Hmm, not working. Oh well! Check in final copy.
+require("rgl")
+require("RColorBrewer")
+plot3d(iris$Petal.Length,  # x variable
+       iris$Petal.Width,   # y variable
+       iris$Sepal.Length,  # z variable
+       xlab = "Petal.Length",
+       ylab = "Petal.Width",
+       zlab = "Sepal.Length",
+       col = brewer.pal(3, "Dark2")[unclass(iris$Species)],
+       size = 8)
+
+
+# Clean up
+detach("package:scatterplot3d", unload = TRUE)
+detach("package:rgl", unload = TRUE)
+detach("package:RColorBrewer", unload = TRUE)
+rm(list = ls())
+dev.off()
