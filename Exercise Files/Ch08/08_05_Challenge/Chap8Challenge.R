b46feba8a85c279f8b1b7c5ff8f29b94e2113522
Loretta Agyemang <loretta.agyemang@gmail.com>
2017-11-22 11:06:58 -0500

    altered things a bit to generate paired scatterplot

--- Exercise Files/Ch08/08_05_Challenge/Chap8Challenge.R
+++ Exercise Files/Ch08/08_05_Challenge/Chap8Challenge.R
@@ -1,35 +1,37 @@
 # Chapter 8 Challenge - Scatterplot Matrix 
 
-# Data: SearchData.csv
+# gssd: Searchgssd.csv
 # Variables: nba, nfl, fifa, degree & age 
 # Graph using "pairs"
 # Graph using "scatterplotMatrix" from "car"
 
 # Access file 
-data <- read.csv('C:\\Users\\loret\\Desktop\\DataSciencePrep\\R\\R_StatisticsEssentialTraining\\Exercise Files\\Ch08\\08_05_Challenge\\SearchData.csv', header = TRUE)
-
+# Note: Call your gssdset something useful (not one of the keywords in R)
+# Save gssdset in object called gssd (Google sports search gssd)
+gssd <- read.csv('C:\\Users\\loret\\Desktop\\gssdSciencePrep\\R\\R_StatisticsEssentialTraining\\Exercise Files\\Ch08\\08_05_Challenge\\Searchgssd.csv', header = TRUE)
 
 # get structure 
-str(data)
-data[1:5, 1:3]
+str(gssd)
+gssd[1:5, 1:3]  # Not bad with trying to access specific variables (nba=2, nfl=3, fifa=4, degree=8 & age=9 )
+gssd[1:5, 2:4 & 8:9]
 
 # Plot to get understanding of distribution
-hist(data$nba)
-hist(data$nfl)
-hist(data$fifa)
-hist(data$degree)
-hist(data$age)
+hist(gssd$nba)
+hist(gssd$nfl)
+hist(gssd$fifa)
+hist(gssd$degree)
+hist(gssd$age) # Left skewed: The outlier - one bin to far left is Utah (relatively young state). 
 
 # Boxplots
-boxplot(data$nba, data$nfl, data$fifa) 
+boxplot(gssd$nba, gssd$nfl, gssd$fifa) 
 # looks like nba has hightest average of searches (based on z-score standardization of search)
 
-boxplot(data$degree, data$age)
+boxplot(gssd$degree, gssd$age)
 # average age seems to be late 30s 
 
-# Access color brewer
+# Access color brewer & car libraries 
 require("RColorBrewer")
-
+require("car")
 
 # Graphing with scatterplots - Standard Scatterplot matrix
 ?pairs
@@ -45,19 +47,19 @@ panel.hist <- function(x, ...)
 }
 
 
-pairs(data[1:4],
+pairs(gssd[1:4],
       panel = panel.smooth,
-      main = "Scatterplot for Google Sports Search Data using Pairs Function",
+      main = "Scatterplot for Google Sports Search gssd using Pairs Function",
       diag.panel = panel.hist,
       pch = 16, 
       col = brewer.pal(3, "Paired"))
 
 # Use the car package to quickly generate a more accessible paired scatterplot
-require("car")
+
 scatterplotMatrix(~nba + nfl + fifa + degree | age,
-                  data = data,
+                  gssd = gssd,
                   col = brewer.pal(3, "RdBu"),
-                  main = "Scatterplot Matrix for Google Sports Search Data Using\n Car Package")
+                  main = "Scatterplot Matrix for Google Sports Search gssd Using\n Car Package")
 
 # Clean up
 ?detach
