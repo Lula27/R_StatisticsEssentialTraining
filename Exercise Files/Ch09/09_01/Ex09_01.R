4653bd196c46e6af5b15c29ac6fa08b132485931
Loretta Agyemang <loretta.agyemang@gmail.com>
2017-11-27 22:27:12 -0500

    started linear model

--- Exercise Files/Ch09/09_01/Ex09_01.R
+++ Exercise Files/Ch09/09_01/Ex09_01.R
@@ -2,3 +2,13 @@
 # Ex09_01
 # Computing a multiple regression
 
+# Load data
+data("USJudgeRatings")
+USJudgeRatings[1:10, ]
+
+str(USJudgeRatings)
+?USJudgeRatings
+
+# Goal: figure out how these 10 factors affect RTEN (y = worthy of retention)
+# regressionName <- lm(y ~ a, b, c, ...)
+reg1 <- lm(RTEN ~ CONT + )
