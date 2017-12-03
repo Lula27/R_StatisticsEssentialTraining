03df501fbbcb747b660b8d6b13709ec0d50a68e9
Loretta Agyemang <loretta.agyemang@gmail.com>
2017-12-02 19:43:12 -0500

    starting off with hierarchical clustering

--- Exercise Files/Ch09/09_03/Ex09_03.R
+++ Exercise Files/Ch09/09_03/Ex09_03.R
@@ -1,6 +1,22 @@
 # R Statistics Essential Training
 # Ex09_03
-# Conducting a cluster analysis
+# Conducting a cluster analysis - group cases based on similarities in senerios in dataset
+# Want to know how long it takes for each of the cars to become relatively 
+# Similar in terms of distance. 
+# To read number of clusters, go accross horizontally at each height level in dendrogram. 
+
 
 ?mtcars
-data(mtcars)
+data(mtcars) # load data
+str(mtcars)
+# View first 5 rows
+mtcars[1:5, ]
+# select variables - 1:4, 6:7, 9:11
+mtcars1 <- mtcars[, c(1:4, 6:7, 9:11)] # all rows, specify columns
+# Look at first 5 rows again
+mtcars1[1:5, ]
+
+# We'll use hierarchical clustering - start separate and combine
+# Need distance matrix (dissimilarity matrix) 
+d <- dist(mtcars1) # Find out how similar/differ each of the cars in each case
+d # Huge matrix - says how similar each of the cars are to each other 
--- Exercise Files/Ch09/09_03/Final/Ex09_03.R
+++ Exercise Files/Ch09/09_03/Final/Ex09_03.R
@@ -1,50 +1,58 @@
 # R Statistics Essential Training
 # Ex09_03
-# Conducting a cluster analysis
+# Conducting a cluster analysis - group cases based on similarities in senerios in dataset
+# Want to know how long it takes for each of the cars to become relatively 
+# Similar in terms of distance. 
+# To read number of clusters, go accross horizontally at each height level in dendrogram. 
+
 
 # Load data
 ?mtcars
 data(mtcars)
 mtcars[1:5, ]
-mtcars1 <- mtcars[, c(1:4, 6:7, 9:11)]  # Select variables
+mtcars1 <- mtcars[, c(1:4, 6:7, 9:11)]  # Select variables - drop some (ie: vs)
 mtcars1[1:5, ]
 
 # Three major kinds of clustering:
-#   1. Split into set number of clusters (e.g., kmeans)
+#   1. Split into set number of clusters (e.g., kmeans) , k - groups
 #   2. Hierarchical: Start separate and combine
 #   3. Dividing: Start with a single group and split
 
 # We'll use hierarchical clustering
 # Need distance matrix (dissimilarity matrix)
-d <- dist(mtcars1)
-d  # Huge matrix
+d <- dist(mtcars1) # Find out how similar/differ each of the cars in each case
+d  # Huge matrix - says how similar each of the cars are to each other 
 
 # Use distance matrix for clustering
-c <- hclust(d)
+c <- hclust(d) # Complete cluster method using Euclidean distance 
 c
 
-# Plot dendrogram of clusters
-plot(c)
+# Plot dendrogram (branches in Greek) of clusters 
+plot(c) # tells you how long it will take for each car to become relatively closer to each other in terms of distance
 
 # Put observations in groups
 # Need to specify either k = groups or h = height
-g3 <- cutree(c, k = 3)  # "g3" = "groups 3"
-# cutree(hcmt, h = 230) will give same result
+g3 <- cutree(c, k = 3)  # "g3" = "groups 3"; split it into 3 clusters 
+# cutree(hcmt, h = 230) will give same result; h = height (y-axis) on dendrogram
 g3
 # Or do several levels of groups at once
 # "gm" = "groups/multiple"
 gm <- cutree(c, k = 2:5) # or k = c(2, 4)
-gm
+gm # numbers mean how many clusters there are
+# Ie: Maserati Bora in different clusters each time (stands alone emperically))
 
-# Draw boxes around clusters
-rect.hclust(c, k = 2, border = "gray")
+# Draw boxes around clusters - see the split between clusters (k = ?)
+# Split is based on similarity in dataset (based on variables in model, different variables would give you different clusters)
+rect.hclust(c, k = 2, border = "red")
 rect.hclust(c, k = 3, border = "blue")
-rect.hclust(c, k = 4, border = "green4")
+rect.hclust(c, k = 4, border = "green4") # Masserati Bora is the only one in it's own cluster
 rect.hclust(c, k = 5, border = "darkred")
 
 # k-means clustering
-km <- kmeans(mtcars1, 3)
-km
+km <- kmeans(mtcars1, 3) # Created 3 clusters w/ different cases (11, 4, 17)
+# Gives means of each cluster on each variable <- used to from a centroid
+# Centroid = center on multiple dimensions (9 variables = 9 dimensional center)
+km   
 
 # Graph based on k-means
 require(cluster)
@@ -56,3 +64,4 @@ clusplot(mtcars1,  # data frame
          labels = 2)  # Labels clusters and cases
 
 rm(list = ls())  # Clean up
+dev.off()
