463eb2ef3142d08db5b44bd4b70d994652ec882c
Loretta Agyemang <loretta.agyemang@gmail.com>
2017-12-02 20:10:10 -0500

    broke into clusters

--- Exercise Files/Ch09/09_03/Ex09_03.R
+++ Exercise Files/Ch09/09_03/Ex09_03.R
@@ -20,3 +20,32 @@ mtcars1[1:5, ]
 # Need distance matrix (dissimilarity matrix) 
 d <- dist(mtcars1) # Find out how similar/differ each of the cars in each case
 d # Huge matrix - says how similar each of the cars are to each other 
+
+
+# Use distance matrix for clustering
+# Complete cluster method using Euclidean distance 
+c <- hclust(d)
+c
+
+# Plot dendrogram (branches in Greek) of clusters 
+# tells you how long it will take for each car to become relatively closer to each other in terms of distance
+plot(c)
+
+
+# Put observations in groups
+# Need to specify either k = groups or h = height
+# "g3" = "groups 3" (k=3); split it into 3 clusters
+g3 <- cutree(c, k=3)
+g3
+
+# Or do several levels of groups at once
+# "gm" = "groups/multiple"
+gm <- cutree(c, k = 2:5) # Clusters 2 - 5
+gm
+
+# Draw boxes around clusters - see the split between clusters (k = ?)
+# Split is based on similarity in dataset (based on variables in model, different variables would give you different clusters)
+rect.hclust(c, k = 2, border = "yellow")
+rect.hclust(c, k = 3, border = "blue")
+rect.hclust(c, k = 4, border = "green4")
+rect.hclust(c, k = 5, border = "red")
