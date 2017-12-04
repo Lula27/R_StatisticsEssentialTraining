0ad833ea6b7e015174cf8ee418b5524a647f503d
Loretta Agyemang <loretta.agyemang@gmail.com>
2017-12-03 19:23:43 -0500

    fixed the issue with the plot

--- Exercise Files/Ch09/09_05_Challenge/Chap9challenge.R
+++ Exercise Files/Ch09/09_05_Challenge/Chap9challenge.R
@@ -3,40 +3,42 @@
 # Include a dendrogram & one with cluster boxes 
 browseURL("http://www.statsoft.com/Textbook/Cluster-Analysis") # Background on cluster analysis
  
-?header
+
 # Load dataset from csv file
-statesResp <- read.csv("C:\\Users\\loret\\Desktop\\DataSciencePrep\\R\\R_StatisticsEssentialTraining\\Exercise Files\\Ch09\\09_05_Challenge\\StateClusterData.csv", header = TRUE)
+stSearch <- read.csv("C:\\Users\\loret\\Desktop\\DataSciencePrep\\R\\R_StatisticsEssentialTraining\\Exercise Files\\Ch09\\09_05_Challenge\\StateClusterData.csv", header = TRUE)
 
-# First 5 rows
-statesResp[1:5, ]
+# First 5 rows - 
+stSearch[1:5, ] 
+# Notice the numbers going down the side / also states are listed
+# We want to restructure it so that state isn't it's own variable but rather, linked to the Google searches
 
-# Drop runny nose (4) and save in new object/dataset
-# sResp <- statesResp[, c(1:4, 6:13)]
-#rm(sResp)
-#sResp[1:5, ]
-# Conduct Hierarchical Cluster Analysis 
+# The reason why my plot didn't come out correctly was because I didn't restructure data set/ 
+# Without that, it'll be just a bunch of numbers, no labels - this fixes that!
+# Restructure dataset 
+# Use first column (state) as rownames 
+rownames(stSearch) <- stSearch[,1]
+stSearch[,1] <- NULL # remove first column (state) because it now has null values
+stSearch[1:5, ]
+
 # Note: 5 clusters based on the column names 
 
 # Step 1: Get distance matrix (dissimilarity matrix)
-d <- dist(statesResp) # Keep getting warning - N
+d <- dist(stSearch) # Notice after dropping the first column, no more warning
 d
 
 # Step 2: Use distance matric for clustering
-c <- hclust(d)
+c <- hclust(d) # hierarchical cluster 
 c
 
 # Step 3: Plot dendrogram of clusters
 plot(c)
 
-# Step 4: Put observations in groups (choosing 4 groups)
-g5 <- cutree(c, k = 5)
-g5
 
-# Step 5: Draw boxes around clusters - see the split between clusters
+# Step 4: Draw boxes around clusters - see the split between clusters
 rect.hclust(c, k = 2, border = "yellow")
 rect.hclust(c, k = 3, border = "green")
-rect.hclust(c, k = 4, border = "red")
-rect.hclust(c, k = 5, border = "purple")
+rect.hclust(c, k = 4, border = "darkred")
+rect.hclust(c, k = 5, border = "blue")
 
 
 # Clean up
