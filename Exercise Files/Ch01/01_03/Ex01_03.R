5950b4351d1f9500869cebf9ef46dc9dfcc10b48
Loretta Agyemang <loretta.agyemang@gmail.com>
2017-12-06 20:24:45 -0500

    playing with vectors

--- Exercise Files/Ch01/01_03/Ex01_03.R
+++ Exercise Files/Ch01/01_03/Ex01_03.R
@@ -1,3 +1,20 @@
 # R Statistics Essential Training
 # Ex01_03
 # Taking a first look at the interface
+
+# Play around with computational skills of R
+a <- c(0, 2, 3, 4, 5)
+b <- c(9, 7, 9, 5, 0)
+
+d <- c(-1, 4, 5, 3, 2)
+
+c <- a + b
+c
+
+e <- d - a
+e
+
+# Descriptive Stats 
+boxplot(a)
+boxplot(b)
+
--- Exercise Files/Ch09/09_06_Solution/Ex09_06_Solution.R
+++ Exercise Files/Ch09/09_06_Solution/Ex09_06_Solution.R
@@ -3,9 +3,10 @@
 # Cluster analysis
 
 # Load data
-scd <- read.csv("~/Desktop/R/StateClusterData.csv", header = TRUE)
+scd <- read.csv("C:\\Users\\loret\\Desktop\\DataSciencePrep\\R\\R_StatisticsEssentialTraining\\Exercise Files\\Ch09\\09_05_Challenge\\StateClusterData.csv", header = TRUE)
+# Restructure dataset so you can effectively perform cluster analysis 
 rownames(scd) <- scd[,1]  # Use state names for row names
-scd[,1] <- NULL  # Remove state names as variable
+scd[,1] <- NULL  # Remove state names as variable (get rid of first column)
 scd[1:5, ]
 
 # We'll use hierarchical clustering
@@ -20,3 +21,4 @@ rect.hclust(c, k = 4, border = "green4")
 rect.hclust(c, k = 5, border = "darkred")
 
 rm(list = ls())  # Clean up
+dev.off()
