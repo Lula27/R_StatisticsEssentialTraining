b7893a02d589757b7dea388907c91916bf68198e
Loretta Agyemang <loretta.agyemang@gmail.com>
2017-12-12 22:26:20 -0500

    cleared bar chart

--- Exercise Files/Ch02/02_02/Ex02_02.R
+++ Exercise Files/Ch02/02_02/Ex02_02.R
@@ -31,3 +31,32 @@ pie(feeds[order(feeds, decreasing = TRUE)],
     main = "Pie Chart of Feeds From Chickwts")
 
 # PROBLEM WITH PIES 
+# Generate 3 datasets
+pie.a <- c(22, 14, 18, 20, 14, 12)
+pie.b <- c(20, 18, 16, 18, 16, 12)
+pie.c <- c(12, 14, 20, 18, 14, 22)
+
+# Changing graphical parameters 
+oldpar <- par() # store old graphical parameters
+par(mfrow = c(1, 3), # Num(row, cols),
+    cex.main = 3) # Main title 3x bigger
+colors <- c("grey98", "grey90", "lightskyblue", "lightgreen", "grey98", "grey90")
+?colors
+
+# Three pie charts side by side - Why aren't these side by side?
+pie(pie.a, main = "Pie A", col = colors)
+pie(pie.b, main = "Pie B", col = colors)
+pie(pie.c, main = "Pie C", col = colors)
+
+# Three bar charts side-by-side
+barplot(pie.a, main = "Bar A", col = colors)
+barplot(pie.b, main = "Bar B", col = colors)
+barplot(pie.c, main = "Bar C", col = colors)
+
+# For some reason, my plots aren't showing up side-by-side
+
+# Restore old graphical parameters
+par(oldpar)
+
+rm(list = ls()) # Clean up
+dev.off() # Clear up graphs
