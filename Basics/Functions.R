d6fd4d8f856be9b7987b296f536bde66034d1a34
Loretta Agyemang <loretta.agyemang@gmail.com>
2017-12-25 16:39:25 -0500

    working with functions in R

---
+++ Basics/Functions.R
@@ -0,0 +1,19 @@
+# Functions 
+
+browseURL("https://nicercode.github.io/guides/functions/") # Source for functions
+
+# Simple function
+
+sum.of.squares <- function(x,y) {
+  x^2 + y^2
+}
+
+sum.of.squares(2,4)
+
+
+add <- function(a,b) {
+  a + b
+}
+
+add(1,2)
+add(206,130)
