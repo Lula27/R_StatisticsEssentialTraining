66bd4e36339dbbff1dce1ecd7dc7931c3ea315b2
Loretta Agyemang <loretta.agyemang@gmail.com>
2017-12-25 18:04:36 -0500

    working with functions

--- Basics/Functions.R
+++ Basics/Functions.R
@@ -1,6 +1,8 @@
 # Functions 
 
-browseURL("https://nicercode.github.io/guides/functions/") # Source for functions
+browseURL("https://nicercode.github.io/guides/functions/") # Source for beginner functions
+browseURL("http://adv-r.had.co.nz/Functions.html") # Advanced functions
+browseURL("https://cran.r-project.org/doc/manuals/R-intro.html#Writing-your-own-functions")
 
 # Simple function
 
@@ -10,10 +12,22 @@ sum.of.squares <- function(x,y) {
 
 sum.of.squares(2,4)
 
-
+# Function to add two numbers
 add <- function(a,b) {
   a + b
 }
 
 add(1,2)
 add(206,130)
+
+# Function to find average
+mean <- function(c,d) {
+  1/2*(c + d)
+}
+
+mean(1,2)
+
+mean(4,4)
+
+
+
