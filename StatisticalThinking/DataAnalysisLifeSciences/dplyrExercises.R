d442bb81c636adbdacd3c4c77867d2609a3e1cbe
Loretta Agyemang <loretta.agyemang@gmail.com>
2018-02-17 18:53:44 -0500

    saved all datasets

--- StatisticalThinking/DataAnalysisLifeSciences/dplyrExercises.R
+++ StatisticalThinking/DataAnalysisLifeSciences/dplyrExercises.R
@@ -5,6 +5,11 @@ browseURL("https://github.com/genomicsclass/labs/blob/master/intro/dplyr_tutoria
 
 # dpylr = powerful R-package that transforms & summarizes tabular data with rows and columns
 
+# get working directory 
+getwd()
+
+# Change working directory: setwd()
+
 # install package
 install.packages("downloader")
 
@@ -15,7 +20,7 @@ url = "https://raw.githubusercontent.com/genomicsclass/dagdata/master/inst/extda
 filename <- basename(url)
 
 
-# Install dplyr
+# Method A: Install dplyr - change mind, just download all class datasets instead
 install.packages("dplyr")
 
  # load dplyr 
@@ -30,4 +35,25 @@ download(url, filename)
 msleep <- read.csv("msleep_ggplot2.csv")
 View(msleep)
 
+rm(msleep)
+
 class(msleep)
+
+
+# Method B: Working through Github - Download the data package
+install.packages("devtools")
+
+# Access library 
+library(devtools)
+install_github("genomicsclass/dagdata") # So files for the class are now on my computer
+
+# Access class files
+dir <- system.file(package="dagdata") # extract location of package
+list.files(dir)
+list.files(file.path(dir, "extdata")) # external data in directory
+
+# Read the file 
+filename <- file.path(dir, "extdata/femaleMiceWeights.csv")
+
+fmweight <- read.csv(filename)  # access dataset 
+View(fmweight)
