be5e7df57160ea670f202f5b5eab58dfc56c9196
Loretta Agyemang <loretta.agyemang@gmail.com>
2018-03-02 11:56:53 -0500

    successfully completed for loop

--- StatisticalThinking/DataAnalysisLifeSciences/assessment1.R
+++ StatisticalThinking/DataAnalysisLifeSciences/assessment1.R
@@ -3,6 +3,7 @@ install.packages("swirl")
 library(swirl)
 
 R.Version()
+?Control
 
 version # this is correct 
 
@@ -18,8 +19,58 @@ for (i in 1:n) {
   print(i)
 }
 
+
+# practice with for loops
+for(i in 1:5) 
+{ 
+  print(1:i)
+}
+  
+
+for(i in 1:10)
+{
+  print(1) # prints out 1, 10 times 
+} 
+
+for(i in 1:5) 
+{
+  print(2*i) # prints (2*1), (2*2), (2*3), (2*4), (2*5)
+}  
+
+# 3 
+i <- 1:25 
+for(i in 1:25) 
+{
+  print(sum(i^2)) 
+}   
+
+# ok...start small. for loop for i^2 up to 3
+a <- 1 + (2)^2 + (3)^2 
+a # a = 14 
+
+# get the for loop to result in 14
+for(i in 1) 
+{
+  i <- 1:3 
+  print(sum(i^2)) 
+}
+
+# correct answer 
+for(i in 1) 
+{
+  i <- 1:25
+  print(sum(i^2)) 
+}
+
+# What exactly is going on here? 
+for(n in c(2,5,10,20,50)) {
+  x <- stats::rnorm(n)
+  cat(n, ": ", sum(x^2), "\n", sep = "")
+}
+
 # 4
 class(cars)
 
-
-?nrows
+#5 - How many rows does the cars object have? 
+?nrow
+nrow(cars)
