50f1e32d3dc6b31d4742664ed69a5626e6ec4467
Loretta Agyemang <loretta.agyemang@gmail.com>
2018-04-08 15:08:44 -0400

    created experiement to determine proportion of times z was larger than 2 in absolute value

--- StatisticalThinking/DataAnalysisLifeSciences/Classwork/Week2/t-distributionExercises.R
+++ StatisticalThinking/DataAnalysisLifeSciences/Classwork/Week2/t-distributionExercises.R
@@ -3,13 +3,14 @@
 
 # Ex: observe proportion of times we see 6 when rolling n=100 die 
 
+set.seed(1)
+
 n <- 100
-n2 <- 1000
 
 x <- sample(1:6, n, replace=TRUE)
 
 # proportion of 6's we get from rolling dice 100x 
-mean(x==6) # 0.24 
+mean(x==6) # 0.24  / with set.seed(1) = 0.14
 
 
 # How about with a coin flip? 
@@ -21,10 +22,30 @@ y
 mean(y==1) # heads = 0.49 
 mean(y==0)
 
-y2 <- sample(0:1, n2, replace=TRUE)
-y2
-mean(y2==1) # heads = 0.528 
-mean(y2==0) # 0.472 
+rm(list = ls())
 
+# 1. What proportion of times z was larger than 2 in absolute value?
+# (CLT says it should be about 0.05
 
+set.seed(1)
 
+n <- 100 # number of events (times dice is rolled)
+
+sides <- 6 # total sides of dice
+
+p <- 1/sides # probability of dice landing on any side
+
+# repeating experiement 10000 times 
+zs <- replicate(10000, {
+  x <- sample(1:sides, n, replace=TRUE)
+  (mean(x==6) - p) / sqrt(p*(1-p)/n)
+})
+
+zs
+
+# create qqnorm plot
+qqnorm(zs)
+abline(0,1) # confirm it's well approximated with normal distribution 
+
+# report what proportion of times z was larger than 2 in absolute value 
+mean(abs(zs) > 2) # 0.0424 
