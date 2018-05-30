b11b11103ea0dc58cdfdbff3c06137e9613aea12
Loretta Agyemang <loretta.agyemang@gmail.com>
2018-05-30 19:26:13 -0400

    progress

--- StatisticalThinking/DataAnalysisLifeSciences/Classwork/Week3_Inference/T-test Exercises.R
+++ StatisticalThinking/DataAnalysisLifeSciences/Classwork/Week3_Inference/T-test Exercises.R
@@ -49,4 +49,33 @@ dat.s <- sample(bwt.smoke, 25)
 
 ?t.test
 tstat <- t.test(dat.ns, dat.s)
-# tstat = 2.1209 
+tstat
+tval <- 2.1209 
+
+# 2. Super vague question...but evidently, find pvalue 
+pval2 <- 1-(pnorm(abs(tval))-pnorm(-abs(tval)))
+pval2
+
+# pval2 = 0.03393022
+
+# 3.  calculate the probability that a t-value under the null could have a larger absolute value than tval
+#a. -0.9660698
+a <- 1-2*pnorm(abs(tval))
+
+# b. 0.9660698
+b <- 1-2*pnorm(-abs(tval))
+
+# c. 0.9830349
+c <- 1-pnorm(-abs(tval))
+
+# d.0.03393022 = CORRECT! :) 
+d <- 2*pnorm(-abs(tval))
+
+# 4. If we use the CLT, what quantity would we add and subtract to this estimate to obtain a 99% confidence interval?
+# sample mean difference = -9.92 
+mean(dat.s) - mean(dat.ns)
+
+# CLT formula - plug & chug - 99% confidence interval = 0.995
+N <- 25 # sample size 
+
+qnorm(0.995)*sqrt( sd( dat.ns)^2/N + sd( dat.s)^2/N )
