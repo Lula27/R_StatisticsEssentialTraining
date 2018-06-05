6495f22d5889502ba6db21051626dcea1297fdbc
Loretta Agyemang <loretta.agyemang@gmail.com>
2018-06-05 14:45:16 -0400

    completed simulations

--- StatisticalThinking/DataAnalysisLifeSciences/Classwork/Week3_Inference/Power Calculations.R
+++ StatisticalThinking/DataAnalysisLifeSciences/Classwork/Week3_Inference/Power Calculations.R
@@ -40,6 +40,48 @@ control <- sample(controlPopulation, N)
 # Run t-test & get p-value: 0.1410204 <- fail to reject null hypothesis that there is no difference btw. two means (mu_hf = mu_control)
 t.test(hf, control)$p.value 
 
+# Power Calculation
+# Power is the probability of rejecting the null when the null is false. 
+# In R, you can use the pwr package to perfom power calculation. 
+?(pwr)
 
+# Sample size 
+N <- 12
 
+# reject the null hypothsis at: 
+alpha <- 0.05
 
+# Number of simulations to run B = 2000
+B <- 2000
+
+# Simulation 
+reject <- function(N, alpha=0.05){
+  hf <- sample(hfPopulation,N) 
+  control <- sample(controlPopulation,N)
+  pval <- t.test(hf,control)$p.value
+  pval < alpha
+}
+# Results of simulation for sample size of 12.
+reject(12)
+
+# Replicate function to do this B times. 
+rejections <- replicate(B, reject(N))
+
+# Our power is just the proportion of times we correctly reject. So with N=12 our power is only:
+mean(rejections) # 0.2155
+
+# Let’s see how power improves with N. 
+# We will use the function sapply, which applies a function to each of the elements of a vector. 
+# We want to repeat the above for the following sample size: 
+Ns <- seq(5, 50, 5)
+
+# Using sapply 
+power <- sapply(Ns, function(N){
+  rejections <- replicate(B, reject(N))
+  mean(rejections)
+}) 
+
+power
+
+# Plot it out 
+plot(Ns, power, type = 'b')
