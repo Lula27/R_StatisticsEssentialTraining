b0eb928a3b9f07dce3cd347e4acc06ac7594d43c
Loretta Agyemang <loretta.agyemang@gmail.com>
2018-06-05 16:12:42 -0400

    completed power calculations work

--- StatisticalThinking/DataAnalysisLifeSciences/Classwork/Week3_Inference/Power Calculations.R
+++ StatisticalThinking/DataAnalysisLifeSciences/Classwork/Week3_Inference/Power Calculations.R
@@ -85,3 +85,124 @@ power
 
 # Plot it out 
 plot(Ns, power, type = 'b')
+
+# Clean up
+rm(list = ls())
+dev.off()
+
+# Power Calculation Exercises 
+babies <- read.table("C:\\Users\\loret\\Desktop\\DataSciencePrep\\R\\R_StatisticsEssentialTraining\\StatisticalThinking\\DataAnalysisLifeSciences\\Classwork\\Week3_Inference\\babies.txt", header=TRUE) 
+
+head(babies)
+
+require(dplyr)
+
+# non smokers population
+bwt.nonsmoke <- filter(babies, smoke==0) %>% select(bwt) %>% unlist
+
+# smokers population
+bwt.smoke <- filter(babies, smoke==1) %>% select(bwt) %>% unlist 
+
+# 2. What proportion of the time do we reject at the 0.05 level?
+# Generate samples 
+N=5
+set.seed(1)
+# non smokers sample 
+dat.ns <- sample(bwt.nonsmoke, N)
+
+# smokers sample 
+dat.s <- sample(bwt.smoke, N)
+
+# Run t-test % get pvalue 
+t <- t.test(dat.s, dat.ns)
+
+t$p.value # 0.1366428 
+
+t$p.value > 0.05 # TRUE = fail to reject null 
+
+# 2. Set the seed at 1, then use the replicate function to repeat the code used in the exercise above 10,000 times. 
+# What proportion of the time do we reject at the 0.05 level? = 0.0984
+N=5
+set.seed(1)
+rejects <- replicate(10000, {
+  dat.ns <- sample(bwt.nonsmoke, N)
+  dat.s <- sample(bwt.smoke, N)
+  t.test(dat.s, dat.ns)$p.value < 0.05
+})
+mean(rejects) # 0.0984  - So we reject the null at the 0.05 level about < 10% of the time. Probably because the sample (N) is only 5.
+
+# mean(rejects) = 0.0984 means power is lower than 10%.
+
+#3. Repeat the exercise above for samples sizes of 30, 60, 90 and 120.
+# Which of those four gives you power of about 80%? 
+N=30
+set.seed(1)
+rejects30 <- replicate(10000, {
+  dat.ns <- sample(bwt.nonsmoke, N)
+  dat.s <- sample(bwt.smoke, N)
+  t.test(dat.s, dat.ns)$p.value < 0.05
+})
+mean(rejects30) # 0.4783 = 47.83% 
+
+
+N=60
+set.seed(1)
+rejects60 <- replicate(10000, {
+  dat.ns <- sample(bwt.nonsmoke, N)
+  dat.s <- sample(bwt.smoke, N)
+  t.test(dat.s, dat.ns)$p.value < 0.05
+})
+mean(rejects60) # 0.7878 = 78.78% 
+
+
+N=90
+set.seed(1)
+rejects90 <- replicate(10000, {
+  dat.ns <- sample(bwt.nonsmoke, N)
+  dat.s <- sample(bwt.smoke, N)
+  t.test(dat.s, dat.ns)$p.value < 0.05
+})
+mean(rejects90) # 0.9338 = 93.38%
+
+N=120
+set.seed(1)
+rejects120 <- replicate(10000, {
+  dat.ns <- sample(bwt.nonsmoke, N)
+  dat.s <- sample(bwt.smoke, N)
+  t.test(dat.s, dat.ns)$p.value < 0.05
+})
+mean(rejects120) # 0.9852 = 98.52% 
+
+# Which of those four gives you power of about 80%? Faster way - use vector
+
+Ns <- c(30, 60, 90, 120)
+
+res <- sapply(Ns, function(N){
+  set.seed(1)
+  rejects <- replicate(10000, {
+    dat.ns <- sample(bwt.nonsmoke, N)
+    dat.s <- sample(bwt.smoke, N)
+    t.test(dat.s, dat.ns)$p.value < 0.05
+  })
+mean(rejects)
+})
+Ns[which.min( abs( res - .8))] # 60 
+
+# 4. Repeat the problem above, but now require an  level of 0.01.
+# Which of those four gives you power of about 80%?
+
+Nt <- c(30, 60, 90, 120)
+
+rep <- sapply(Nt, function(N){
+  set.seed(1)
+  rejects <- replicate(10000, { 
+    dat.ns <- sample(bwt.nonsmoke, N)
+    dat.s <- sample(bwt.smoke, N)
+    t.test(dat.s, dat.ns)$p.value < 0.01
+    })
+  mean(rejects)
+})
+Nt[which.min( abs( rep - .8))] # 90
+
+# Clean up 
+rm(list = ls())
