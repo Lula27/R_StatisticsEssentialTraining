b3bda776df3d48ba7a09d9507cebb517df8d0b0e
Loretta Agyemang <loretta.agyemang@gmail.com>
2018-06-07 12:21:23 -0400

    completed wilcoxon test examples

--- StatisticalThinking/DataAnalysisLifeSciences/Classwork/Week5_ExploratoryDataAnalysis/RobustSummaries.R
+++ StatisticalThinking/DataAnalysisLifeSciences/Classwork/Week5_ExploratoryDataAnalysis/RobustSummaries.R
@@ -85,8 +85,47 @@ b/a # 2.370719
 
 # Mann-Whitney-Wilcoxon Test Exercises 
 #1. Perform a t-test of x and y, after adding a single chick of weight 200 grams to x (the diet 1 chicks)
-# What is the p-value from this test? 
-x <- chick$weight.4[chick$Diet == 1]
-y <- chick$weight.4[chick$Diet == 4]
+# What is the p-value from this test? : 0.9380347 - fail to reject null
+x <- chick$weight.4[chick$Diet == 1] # Chick weight on day 4 for chicks on diet 1
+y <- chick$weight.4[chick$Diet == 4] # Chick weight on day 4 for chicks on diet 4
 t.test(c(x, 200), y)$p.value
 
+# 2. Do the same for the Wilcoxon test.
+# What is the p-value from this test? 
+# In addition, it has less assumptions that the t-test on the distribution of the underlying data.
+wilcox.test(x,y,exact = FALSE)$p.value # Without outlier - 0.0002011939 - reject null 
+
+# With outlier 
+wilcox.test(c(x,200),y,exact = FALSE)$p.value # 0.0009840921 - Even with outlier, still reject null
+
+# Down side to Wilcoxon-Mann-Whitney test statistic 
+# In certain contexts, Wicoxon test is less powerful than t-test. (ie: small sample sizes)
+# Make 3 boxplots using the following code 
+library(rafalib)
+mypar(1,3) # 1 frame, 3 panels
+boxplot(x,y) # original 
+boxplot(x,y+10) # w/ outlier y shiffed up 10 
+boxplot(x,y+100) # w/ outlier y shiffed up 100 
+
+# 3. What is the difference in t-test statistic btw. adding 10 and adding 100 to all the values in the group 'y'?
+c <- t.test(x,y+10)$statistic # t-test statistic value
+c # -13.36722 
+
+d <- t.test(x,y+100)$statistic # -81.11819
+d 
+
+# Difference 
+c-d # 67.75097
+
+# 4. What is the p-value if we compare c(1,2,3) to c(4,5,6) using a Wilcoxon test?
+i <- c(1,2,3)
+j <- c(4,5,6)
+
+wilcox.test(i,j)$p.value # 0.1
+
+# What is the p-value if we compare c(1,2,3) to c(400,500,600) using a Wilcoxon test?
+k <- c(400,500,600)
+
+wilcox.test(i,k)$p.value # 0.1 
+
+# NOtice that there is no difference even going up in sample size by a factor of 100...
