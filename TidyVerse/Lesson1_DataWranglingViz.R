b86d7d7558843130cd26042d3da26596b164edc0
Loretta Agyemang <loretta.agyemang@gmail.com>
2019-03-22 17:22:50 -0500

    completed clustering exercises

--- TidyVerse/Lesson1_DataWranglingViz.R
+++ TidyVerse/Lesson1_DataWranglingViz.R
@@ -1,5 +1,6 @@
 # Install packages & libraries 
 
+browseURL("https://uoftcoders.github.io/rcourse/lec04-dplyr.html#lesson_preamble")
 install.packages('tidyverse')
 library(tidyverse)
 
@@ -77,12 +78,13 @@ surveys %>%
             max_hindfoot_length = max(hindfoot_length))
 
 # Year level analysis 
-surveys %>%
+years <- surveys %>%
   filter(!is.na(weight)) %>%
   group_by(year) %>%
   filter(weight == max(weight)) %>% 
   select(year, genus, species, weight) %>%
   arrange(year)
+
 
 # Tally used to summarize categorical data 
 surveys %>% 
@@ -129,4 +131,104 @@ surveys %>%
 surveys %>% 
   group_by(sex) %>%
   summarize(n = n())
+
+# Plotting with ggplot2 
+ggplot(data = surveys)
+
+# If arguments in right order, you can omit argument names 
+ggplot(surveys)
+
+# Add aesthetics (aes)
+ggplot(surveys, aes(x = weight, y = hindfoot_length))
+
+# Create subset for quicker plotting
+survey_subset <- sample_n(surveys, size = 5000)
+
+head(survey_subset)
+
+# Plot weight against hindfoot length 
+ggplot(survey_subset, aes(x = weight, y = hindfoot_length)) + 
+  geom_point()
+
+# Plot years - time series 
+ggplot(years, aes(x = year, y = weight)) + 
+  geom_line()
+
+# Add transparancy (alpha) to reduce overplotting 
+ggplot(data = survey_subset, aes(x = weight, y = hindfoot_length)) + 
+  geom_point(alpha = 0.2)
+
+# Add color - look familiar? ... clustering ho! 
+#  Coloring the data points according to a categorical variable is an easy way to find out if there seems to be correlation.  
+ggplot(survey_subset, aes(x = weight, y = hindfoot_length, color = plot_type)) + 
+  geom_point(alpha = 0.2)
+
+# Based on plot, create variable with 4-5 values to help explain observed pattern in scatter plot 
+surveys %>% 
+  summarize_all(n_distinct)
+
+# Sex = 3 b/c there are still NAs 
+
+# Use taxa cus there are only 4 of those 
+surveys %>% 
+  distinct(taxa)
+
+# Use categorical variable to color scatterplot - looks like just 1 cluster with rodent 
+ggplot(survey_subset, aes(x = weight, y = hindfoot_length, color = taxa)) + 
+  geom_point()
+
+# Figure out what's happening by grouping using taxa 
+# Rodent classification dominates - most animals in dataset = roadents
+surveys %>% 
+  group_by(taxa) %>% 
+  tally() %>%
+  arrange(desc(n))
+
+# Review data for hindfoot measurements 
+surveys %>% 
+  filter(!is.na(hindfoot_length)) %>%
+  group_by(taxa) %>%
+  tally()
+
+# Remove all animals that haven't had hindfeet measured - including rodents that didn't 
+surveys_hf_wt <- surveys %>%
+  filter(!is.na(hindfoot_length) & !is.na(weight))
+
+surveys_hf_wt %>% 
+  summarize_all(n_distinct)
+
+# Try classifying (grouping) with genus 
+ggplot(surveys_hf_wt, aes(x = weight, y = hindfoot_length, color = genus)) + 
+  geom_point(alpha = 0.2)
+
+# Separate observations into different species 
+# Looks like 5 clusters but 21 species in legend 
+ggplot(surveys_hf_wt, aes(x = weight, y = hindfoot_length, color = species)) + 
+  geom_point(alpha = 0.2)
+
+surveys %>%  
+  filter(!is.na(hindfoot_length) & !is.na(weight)) %>% 
+  group_by(species) %>%
+  tally() %>%
+  arrange(desc(n))
+
+# Only include species with more than 800 observations 
+# only about 10k rows removed 
+surveys_abun_species <- surveys %>% 
+  filter(!is.na(hindfoot_length) & !is.na(weight)) %>%
+  group_by(species) %>% 
+  mutate(n = n()) %>% # add count value to each row 
+  filter(n > 800) %>% 
+  select(-n)
+
+surveys_abun_species
+
+# Plot about 25k observations 
+ggplot(surveys_abun_species, aes(x = weight, y = hindfoot_length, color = species)) + 
+  geom_point(alpha = 0.2)
+
+# Create scatterplot of hindfoot_length over species with weight showing in different colors 
+ggplot(surveys_abun_species, aes(x = weight, y = species, color = hindfoot_length)) + 
+  geom_point(size = 0.1, position = 'jitter')
+
 
